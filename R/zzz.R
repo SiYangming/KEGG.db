afec5794586bf36b3acb7f08865be11cb6fff416
SiYangming <yangming-6863@qq.com>
2021-02-05 22:28:49 +0800

    KEGG.db_3.2.4

---
+++ R/zzz.R
@@ -0,0 +1,32 @@
+datacache <- new.env(hash=TRUE, parent=emptyenv())
+
+KEGG <- function() showQCData("KEGG", datacache)
+KEGG_dbconn <- function() dbconn(datacache)
+KEGG_dbfile <- function() dbfile(datacache)
+KEGG_dbschema <- function(file="", show.indices=FALSE) dbschema(datacache, file=file, show.indices=show.indices)
+KEGG_dbInfo <- function() dbInfo(datacache)
+
+.onLoad <- function(libname, pkgname)
+{
+    ## Connect to the SQLite DB
+    dbfile <- system.file("extdata", "KEGG.sqlite", package=pkgname, lib.loc=libname)
+    assign("dbfile", dbfile, envir=datacache)
+    dbconn <- dbFileConnect(dbfile)
+    assign("dbconn", dbconn, envir=datacache)
+    ## Create the AnnObj instances
+    ann_objs <- createAnnObjs.SchemaChoice("KEGG_DB", "KEGG", "KEGG", dbconn, datacache)
+    mergeToNamespaceAndExport(ann_objs, pkgname)
+    packageStartupMessage(AnnotationDbi:::annoStartupMessages("KEGG.db"))
+}
+
+.onUnload <- function(libpath)
+{
+    dbFileDisconnect(KEGG_dbconn())
+}
+
+.onAttach <- function(libname, pkgname) {
+    msg <- sprintf(
+        "Package '%s' is deprecated and will be removed from Bioconductor
+         version %s", pkgname, "3.12")
+    .Deprecated(msg=paste(strwrap(msg, exdent=2), collapse="\n"))
+}
